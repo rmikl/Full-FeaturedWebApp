caf5ec6f649f88b98072a7b156ea2e1b566d50d2
robertmpl <robertm131@gmail.com>
2019-03-20 14:35:08 -0700

    added registration login sites, post method for registration with return name with confirmation message

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, flash, redirect
+from forms import LoginForm, RegistrationForm
+
 
 
 app = Flask(__name__)
-
+app.config['SECRET_KEY'] = '74eed421a8148c400b26d6f3e2319147'
 
 
 posts = [
@@ -21,14 +23,30 @@ posts = [
 ]
 
 
+
 @app.route("/")
 @app.route("/home")
-def hello():
+def home():
     return render_template('home.html', title='Strona Glowna')
 
 @app.route("/about")
 def about():
     return render_template('about.html', posts=posts)
+
+@app.route("/register", methods=['GET','POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+            flash( str({form.username.data}) + 'zostal poprawnie zarejestowany!', 'success')
+            return(redirect(url_for('home')))
+    return render_template('register.html', title='Zarejestruj', form=form)
+
+@app.route("/login")
+def login():
+    form = LoginForm()
+    return render_template('login.html', title='Zaloguj', form=form)
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
